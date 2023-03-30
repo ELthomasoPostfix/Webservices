b8790208f49fb30a464b6ff947e08fc823736ac4
elthomaso <GueutalThomas@outlook.com>
2023-03-30 15:56:18 +0200

    Update movies collection API
    * Change getPopularPage return type/value
    * Replace raw 'make_response' calls
     because style

--- API/Movies.py
+++ API/Movies.py
@@ -1,6 +1,9 @@
-from flask_restful import Resource, reqparse, current_app
-from math import ceil
 import requests
+from requests.exceptions import JSONDecodeError, InvalidJSONError
+from flask_restful import Resource, reqparse, current_app
+
+from .APIResponses import make_response_message, make_response_error, GenericResponseMessages as E_MSG
+
 
 class MoviesParameters(object):
     """An enum of the parameters used by the Movies resource
@@ -42,46 +45,68 @@ class Movies(Resource):
         return "/movies"
 
     @staticmethod
-    def getPopularPage(page: int) -> dict:
-        """Get a page of popular movies from the TMDB /movie/popular API.
-
+    def getPopularPage(page: int) -> requests.Response:
+        """Get a *page* of popular movies from the TMDB ``/movie/popular`` API.
+        
         :param page: Which page to retrieve
-        :return: The page in json format if successful, else an empty dict
+        :return: The page in json format if successful, else ``{}``
         """
         # guard clause, based on TMDB page param requirements
         if page < 1 or page > 1000:
             return {}
-        return requests.get(f"https://api.themoviedb.org/3/movie/popular?page={page}&api_key={current_app.config['API_KEY_TMDB']}").json()
+        return requests.get(f"https://api.themoviedb.org/3/movie/popular?page={page}&api_key={current_app.config['API_KEY_TMDB']}")
 
     def get(self):
+        """The filter/search endpoint of the collection of all movies.
+
+        The filtering system works on the principle of a whitelist. The
+        base response is the empty dict, ``{}``. A base set of movies
+        needs to be selected, e.g. the x most popular movies. Adding
+        filters via the query string allows narrowing donw the results.
+
+        :return: The result of the filter query, ``{}`` by default
+        """
         args = parser.parse_args()
 
-        popular_x: int | None = args[MoviesParameters.popular]
-        result: dict = dict()
+        try:
+            popular_x: int | None = args[MoviesParameters.popular]
+            result: dict = dict()
 
-        if popular_x is not None:
-            assert popular_x >= 0, f"The {MoviesParameters.popular} parameter may not be negative"
+            if popular_x is not None:
+                if popular_x < 0:
+                    return make_response_error(E_MSG.MALFORMED_REQ,
+                                               f"The {MoviesParameters.popular} parameter must be positive",
+                                               400)
 
-            popular_x_movies = []
-            remaining_movies: int = popular_x
-            total_pages_available: int = 1
-            current_page: int = 1
+                popular_x_movies = []
+                remaining_movies: int = popular_x
+                total_pages_available: int = 1
+                current_page: int = 1
 
-            while remaining_movies > 0 and current_page <= total_pages_available:
-                # Query TMDB API
-                # Has Protection against change in pagecount during long query (large popularx)
-                tmdb_page: dict = self.getPopularPage(page=current_page)
-                total_pages_available = tmdb_page["total_pages"]
-                results = tmdb_page["results"][:remaining_movies]
+                while remaining_movies > 0 and current_page <= total_pages_available:
+                    # Query TMDB API
+                    # Has Protection against change in pagecount during long query (large popularx)
+                    tmdb_resp = self.getPopularPage(page=current_page)
 
-                # Bookkeeping
-                remaining_movies -= len(results)
-                current_page += 1
-
-                # Append results
-                popular_x_movies.extend(results)
-
-            result[MoviesParameters.popular] = popular_x_movies
+                    if not tmdb_resp.ok:
+                        return make_response_error(E_MSG.ERROR, "TMDB raised an exception while fetching a popular page", 500)
 
 
-        return result
+                    tmdb_resp_json = tmdb_resp.json()
+                    total_pages_available = tmdb_resp_json["total_pages"]
+                    results = tmdb_resp_json["results"][:remaining_movies]
+
+                    # Bookkeeping
+                    remaining_movies -= len(results)
+                    current_page += 1
+
+                    # Append results
+                    popular_x_movies.extend(results)
+
+                result[MoviesParameters.popular] = popular_x_movies
+        except JSONDecodeError as e:
+            return make_response_error(E_MSG.ERROR, "TMDB gave an invalid response", 502)
+        except Exception as e:
+            return make_response_error(E_MSG.UNEXPECTED, "Unexpected exception while querying the Movies collection", 500)
+
+        return make_response_message(E_MSG.SUCCESS, 200, result=result)
