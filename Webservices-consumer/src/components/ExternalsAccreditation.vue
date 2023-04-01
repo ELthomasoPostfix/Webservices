c52fb35c4d333f8d1301b2ad704b331b214ea782
elthomaso <GueutalThomas@outlook.com>
2023-04-01 21:33:11 +0200

    Reorganize styles
    * Make footer positioning consistent
    * Add top margin to external accreditation

--- Webservices-consumer/src/components/ExternalsAccreditation.vue
+++ Webservices-consumer/src/components/ExternalsAccreditation.vue
@@ -5,18 +5,23 @@ import TMDBLogo from './logos/TMDBLogo.vue';
 </script>
 
 <template>
-
-  <!-- TMDB accreditation -->
-  <TMDBLogo/>
-  <!-- Credit notice -->
-  <p class="credit-text">
-    <b>This product uses the TMDB API but is not endorsed or certified by TMDB.</b>
-  </p>
-  <br>
+  <div class="credit-container">
+    <!-- TMDB accreditation -->
+    <TMDBLogo/>
+    <!-- Credit notice -->
+    <p class="credit-text">
+      <b>This product uses the TMDB API but is not endorsed or certified by TMDB.</b>
+    </p>
+    <br>
+  </div>
 
 </template>
 
 <style setup>
+.credit-container {
+  margin-top: 1rem;
+}
+
 .credit-text {
   display: inline;
 }
