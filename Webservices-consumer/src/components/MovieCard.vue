7dafbf61fd893d2e8011222b09c6c767811cfa04
elthomaso <GueutalThomas@outlook.com>
2023-04-01 14:59:27 +0200

    Create and apply MovieCard.
    The MovieCard component is a simple
     card representing a single fetched movie
     for the movies page.
    
    * Diplay a MovieCard on the Movies view
     for each fetched movie.
    * Update main.css and Movies view UI/styles

---
+++ Webservices-consumer/src/components/MovieCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+    /** The movie's title */
+    title: String
+}>();
+</script>
+
+<template>
+    <div class="movie-card">
+        <h6 class="movie-card-title">
+            {{ title }}
+        </h6>
+    </div>
+</template>
+
+<style scoped>
+.movie-card {
+  border: 2px solid;
+  border-radius: 10px;
+  display: grid;
+}
+
+.movie-card-title {
+  padding: 20px;
+}
+</style>
--- Webservices-consumer/src/views/MoviesView.vue
+++ Webservices-consumer/src/views/MoviesView.vue
@@ -3,6 +3,7 @@ import { ref, type Ref, computed } from "vue";
 import type { Movie } from "../code/interfaces";
 
 import MainContentHeader from "@/components/MainContentHeader.vue";
+import MovieCard from "../components/MovieCard.vue";
 
 
 /** Popular x input state */
@@ -51,20 +52,50 @@ function onClick() {
 
 <template>
   <main>
+    <!-- View header -->
     <MainContentHeader title="Movies"/>
 
-    <!-- Movies view content -->
-    <form @submit.prevent="">
-      <label style="padding-right: 1rem;" for="input-popularX">First {{ popular_x_repr_string }} popular movies</label>
-      <input id="input-popularX" type="number" min="0" v-model="popular_x"/>
+    <!-- View content -->
+    <form @submit.prevent=""
+      style="margin-top: 1rem; margin-bottom: 1rem;"
+    >
+      <label for="input-popularX" style="padding-right: 1rem;">First X popular movies</label>
+      <input style="margin-right: 1rem; max-width: 100px;" id="input-popularX" type="number" min="0" v-model="popular_x"/>
       <button id="button-popularX" @click="onClick">Get popular {{ popular_x_repr_string }}</button>
     </form>
 
     <p v-if="popular_x_data.length == 0" id="results-popularX">No results yet</p>
-    <ul v-else>
-      <li v-for="(value) in popular_x_data" :key="value.title">
-        {{ value.title }}
-      </li>
-    </ul>
-  </main>
+    <!-- List of fetched movies -->
+    <div v-else class="movie-grid">
+      <div v-for="(value, index) in popular_x_data" :key="value.title">
+        <MovieCard class="grid-element" :title="`${index+1}. ${value.title}`"/>
+      </div>
+    </div>
+</main>
 </template>
+
+<style scoped>
+.grid-element {
+  height: 100%;   /** Force movie cards on the same row to be equally tall */
+}
+
+.movie-grid {
+  display: grid;
+  grid-template-columns: 1fr;   /** Default one column of 1 fraction of 100% space */
+  grid-template-rows: 1fr;      /** Default 1 row of 1 fraction of 100% space */
+  column-gap: 1rem;
+  row-gap: 1rem;
+}
+
+@media (min-width: 512px) {
+  .movie-grid {
+    grid-template-columns: 1fr 1fr;   /** If available space wide enough, 2 columns of 1 fraction of 100% each, meaning 50% each */
+  }
+}
+
+@media (min-width: 1280px) {
+  .movie-grid {
+    grid-template-columns: 1fr 1fr 1fr;   /** If available space wide enough, 3 columns of 1 fraction of 100% each, meaning 33.33% each */
+  }
+}
+</style>
