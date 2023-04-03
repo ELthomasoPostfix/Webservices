eedbb1bd19593ae6aca3820ae48a6731420d73d8
elthomaso <GueutalThomas@outlook.com>
2023-04-03 13:24:56 +0200

    Fix overindented return

--- API/Movies.py
+++ API/Movies.py
@@ -104,7 +104,7 @@ class Movies(Resource):
 
                 result[MoviesParameters.popular] = popular_x_movies
 
-                return make_response_message(E_MSG.SUCCESS, 200, result=result)
+            return make_response_message(E_MSG.SUCCESS, 200, result=result)
         except JSONDecodeError as e:
             return make_response_error(E_MSG.ERROR, "TMDB gave an invalid response", 502)
         except Exception as e:
