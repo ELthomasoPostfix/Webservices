b53016363fea6f007dec0b6acc10061d1e31f992
elthomaso <GueutalThomas@outlook.com>
2023-04-03 16:35:35 +0200

    Add GET/DELETE for individual movies.
    * a DELETE on the
     endpoint removes a movie from all endpoint's
     reponses until API restart
    * a GET on the same endpoint retrieves the primary
     movie info from TMDB for that movie,
     unless it has been deleted

--- API/Movie.py
+++ API/Movie.py
@@ -1,5 +1,12 @@
-from flask_restful import Resource
+from json import JSONDecodeError
+import requests
+from flask_restful import Resource, current_app
+
+from .utils import catch_unexpected_exceptions
+from .MovieAttributes import MovieAttributes
 from .Movies import Movies
+from .APIResponses import GenericResponseMessages as E_MSG, make_response_error, make_response_message
+
 
 class Movie(Resource):
     """The api endpoint that represents a single movie resource.
@@ -12,5 +19,53 @@ class Movie(Resource):
         """
         return f"{Movies.route()}/<int:mov_id>"
 
+    @staticmethod
+    def getMovie(movie_id: int) -> requests.Response:
+        """Get the primary information about a movie from the TMDB ``/movie/{movie_id}`` API.
+
+        :param movie_id: Which movie's information to retrieve
+        :return: The movie's primary information in json format if successful, else ``{}``
+        """
+        return requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={current_app.config['API_KEY_TMDB']}")
+
+    @catch_unexpected_exceptions("fetch a movie's primary information")
     def get(self, mov_id: int):
-        return f"movie {mov_id}"
+        """The query endpoint of the primary information for a single, specific movie resource.
+
+        :return: The movie's 'deleted' status and, if not deleted, its primary information
+        """
+        from . import movies_attributes
+
+        # Do not return deleted movies
+        is_deleted: bool = movies_attributes.get(mov_id, MovieAttributes(deleted=False)).deleted
+        if is_deleted:
+            return make_response_message(E_MSG.SUCCESS, 204)
+
+        # Query TMDB API
+        # Has Protection against change in pagecount during long query (large popularx)
+        tmdb_resp = self.getMovie(movie_id=mov_id)
+        if not tmdb_resp.ok:
+            return make_response_error(E_MSG.ERROR, "TMDB raised an exception while fetching a movie's primary information", 500)
+
+        try:
+            return make_response_message(E_MSG.SUCCESS, 200, result=tmdb_resp.json(), deleted=is_deleted)
+        except JSONDecodeError as e:
+            return make_response_error(E_MSG.ERROR, "TMDB gave an invalid response", 502)
+
+    @catch_unexpected_exceptions("deleting a movie")
+    def delete(self, mov_id: int):
+        """The delete endpoint for a single, specific movie resource.
+
+        All deleted movies are excluded from being returned by any of
+        the related API endpoints.
+
+        :return: None
+        """
+        from . import movies_attributes
+
+        if mov_id in movies_attributes:
+            movies_attributes[mov_id] = MovieAttributes(deleted=True)
+        else:
+            movies_attributes[mov_id].deleted = True
+
+        return make_response_message(E_MSG.SUCCESS, 200)
