bc30b85447010021222cc4104b5125bef95a67de
elthomaso <GueutalThomas@outlook.com>
2023-04-05 17:02:02 +0200

    Add standalone placeholder for movie cards.
    The placeholder for movie cards copies the
     styles of the original MovieCard as a one
     time off. The placeholder component is
     meant as page filler content and is thus
     easier to extend and handle if it is
     completely separate from the original
     component, though it is harder to maintain.
    Also added a PlaceholderRect component that
     is a simple grey svg rect. It represents
     to-be-loaded content.

---
+++ Webservices-consumer/src/components/MoviePlaceholderCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import PlaceholderRect from './PlaceholderRect.vue';
+</script>
+
+<template>
+  <div class="movie-card">
+    <!-- Card header -->
+    <h6 class="movie-card-title">
+      <PlaceholderRect :width="70" :height="20"/>
+    </h6>
+    <!-- Card content -->
+    <div class="movie-card-body" style="margin-left: 1rem;">
+      <PlaceholderRect :width="30" :height="10"/>
+      <PlaceholderRect style="margin-left: 1rem;" :width="10" :height="10"/>
+      <br>
+      <PlaceholderRect :width="45" :height="10"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.movie-card {
+  border: 2px solid;
+  border-radius: 10px;
+  display: grid;
+}
+
+.movie-card-title {
+  margin: 10px;
+}
+
+.movie-card-body {
+  margin-bottom: 5px;
+}
+</style>
---
+++ Webservices-consumer/src/components/PlaceholderRect.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+    /** The width of the placeholder rect as a percentage,
+     * e.g. width="30" will take up 30% of available width
+     */
+    width: number;
+    /** The height of the placeholder rect in pixels,
+     * e.g. height="30" will be 30px high
+     */
+     height: number;
+}>();
+</script>
+
+<template>
+  <svg :width="`${width}%`" :height="`${height}px`" class="placeholder-rect">
+    <rect width="100%" height="100%" />
+  </svg>
+</template>
+
+<style scoped>
+.placeholder-rect {
+  fill: rgb(179, 173, 173);
+  stroke-width: 3;
+  border-radius: 1rem;
+
+  margin: 0;
+}
+</style>
