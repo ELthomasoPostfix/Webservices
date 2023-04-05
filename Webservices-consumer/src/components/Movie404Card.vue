a527e4b65d7021b0beffa2aa700fb9aa521cf927
elthomaso <GueutalThomas@outlook.com>
2023-04-05 17:03:36 +0200

    Add standalone 404 representation of MovieCard.
    This component is designed to receive
     minimal props, as a 404 response would
     leave the user with little movie data
     to present.

---
+++ Webservices-consumer/src/components/Movie404Card.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+
+import MovieCard from './MovieCard.vue';
+
+/** The props should take into account that the represented movie is an invalid TMDB resource.
+ *
+ * A user of this component would normally only be able to provide limited information, such
+ * as the movie's TMDB id.
+ */
+const props = defineProps<{
+    /** The movie TMDB id */
+    movieId: number
+}>();
+
+/** The '404 Not Found' representation of the movie data */
+const movie = reactive({
+    title: "Movie Not Found",
+    id: props.movieId
+});
+</script>
+
+<template>
+    <MovieCard class="movie-404-card" :movie="movie" v-bind="$attrs"/>
+</template>
+
+<style scoped>
+.movie-404-card {
+    border-color: red;
+}
+</style>
