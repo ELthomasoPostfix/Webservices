b19a7d3871ce5197c8e67a1e4528c8c0b719f6c7
elthomaso <GueutalThomas@outlook.com>
2023-04-05 16:57:36 +0200

    Extend MovieCard's content.
    The MovieCard now extracts and renders
     content part of the passed movie if
     possible.
    The styles have also been adjusted
     accordingly.

--- Webservices-consumer/src/components/MovieCard.vue
+++ Webservices-consumer/src/components/MovieCard.vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
 import type { Movie } from '../code/interfaces';
+
 defineProps<{
     /** The movie to display */
-    movie: Movie
+    movie: Movie;
 }>();
 </script>
 
 <template>
-    <div class="movie-card">
-        <h6 class="movie-card-title">
-            {{ movie.title }}
-        </h6>
+  <div class="movie-card">
+    <!-- Card header -->
+    <h6 class="movie-card-title">
+      <div>
+        {{ movie.title }}
+      </div>
+    </h6>
+    <!-- Card content -->
+    <div class="movie-card-body ">
+      <p>
+        Movie ID: {{ movie.id }}
+      </p>
+      <p v-if="movie.runtime !== undefined">
+        Runtime: {{ movie.runtime }}
+      </p>
+      <p v-if="movie.genre_ids !== undefined">
+        Genre IDs: {{ movie.genre_ids.join(", ") }}
+      </p>
     </div>
+  </div>
 </template>
 
 <style scoped>
@@ -22,6 +38,15 @@ defineProps<{
 }
 
 .movie-card-title {
-  padding: 20px;
+  margin: 10px;
+}
+
+.movie-card-body {
+  margin-bottom: 5px;
+}
+
+p {
+  margin-left: 1rem;
+  margin-bottom: 0rem;
 }
 </style>
