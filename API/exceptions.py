bfa6fe8324dae85d90f9404d4583e6e91227d0bc
elthomaso <GueutalThomas@outlook.com>
2023-04-08 15:42:42 +0200

    Add exception classes for TMDB and quickcharts.
    These custom exceptions are for the
     specific purpose of handling NotOK
     exceptions.

--- API/exceptions.py
+++ API/exceptions.py
@@ -9,3 +9,13 @@ class NotOKError(requests.HTTPError):
     """A generic error representing an exception to be thrown
     in case a `requests.Response.ok` property is false."""
     pass
+
+
+class NotOKTMDB(NotOKError):
+    """A specification of a NotOKError for the TMDB API"""
+    pass
+
+
+class NotOKQuickchart(NotOKError):
+    """A specification of a NotOKError for the quickchart API"""
+    pass
