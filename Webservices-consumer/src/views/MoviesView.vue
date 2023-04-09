76db0e3830a61cc836405eb674144e512b315b7f
elthomaso <GueutalThomas@outlook.com>
2023-04-09 23:35:21 +0200

    Disassociate popularx from movies page.
    Previously, the movies page provided access
     to the popularx functionality of the
     Webservices API. Relate the movies page to
     the  api instead. It simply
     fetches the requested amount of normal movies,
     not popular movies specifically.
    
    Refactor the code to reflect its new purpose.
    Adjust the text on the movies page to reflect
     its new purpose.

--- Webservices-consumer/src/views/MoviesView.vue
+++ Webservices-consumer/src/views/MoviesView.vue
@@ -6,25 +6,15 @@ import MainContentHeader from "@/components/MainContentHeader.vue";
 import MovieGrid from "@/components/MovieGrid.vue";
 
 
-/** Popular x input state */
-const popular_x: Ref<number> = ref(0);
+/** Movies input state */
+const amount: Ref<number> = ref(0);
 
-/** Popular x api results */
-const popular_x_data: Array<Movie> = reactive([]);
+/** Movies Webservices api results */
+const movies_data: Array<Movie> = reactive([]);
 
-/** Transformed popular x api results, suitable to be displayed */
-const displayable_data = computed(() => {
-  return popular_x_data.map((elem: Movie, index: number) => {
-    return {
-      ...elem,
-      title: `${index+1}. ${elem.title}`
-    }
-  })
-})
-
-/** Popular x representation string for display purposes */
-const popular_x_repr_string = computed(() => {
-  return popular_x.value > 0 ? popular_x.value.toString() : 'X';
+/** Movies amount representation string for display purposes */
+const amount_repr_string = computed(() => {
+  return amount.value > 0 ? amount.value.toString() : 'X';
 })
 
 /** The response format of the `/movies` api endpoint. */
@@ -37,9 +27,9 @@ interface MoviesResponse {
   result: Array<Movie>;
 }
 
-/** Fetch the first x popular movies and update the state variable upon success */
+/** Fetch the first x movies and update the state variable upon success */
 function onClick() {
-  fetch(`${import.meta.env.VITE_APP_API_BASE_URL}/movies/popular?amount=${popular_x.value}`, {
+  fetch(`${import.meta.env.VITE_APP_API_BASE_URL}/movies?amount=${amount.value}`, {
       credentials: "same-origin"
     })
   .then(async (response) => {
@@ -51,8 +41,8 @@ function onClick() {
         const result = response_json["result"];
 
         /** Unpack response data */
-        popular_x_data.splice(0);
-        popular_x_data.push(...result);
+        movies_data.splice(0);
+        movies_data.push(...result);
       });
   })
   .catch((e) => {
@@ -65,8 +55,9 @@ function onClick() {
  *
  * Update the like status of this page's movies state.
  * @param movie The movie to update the liked state of
- */async function onTriggerLike(movie: Movie) {
-  popular_x_data[displayable_data.value.indexOf(movie)].liked = !movie.liked;
+ */
+async function onTriggerLike(movie: Movie) {
+  movie.liked = !movie.liked;
 }
 
 /** Event handler for the 'delete' event.
@@ -75,7 +66,7 @@ function onClick() {
  * @param movie The movie to remove from the page's movies state
  */
 async function onTriggerDelete(movie: Movie) {
-  popular_x_data.splice(displayable_data.value.indexOf(movie), 1);
+  movies_data.splice(movies_data.indexOf(movie), 1);
 }
 </script>
 
@@ -88,14 +79,14 @@ async function onTriggerDelete(movie: Movie) {
     <form @submit.prevent=""
       style="margin-top: 1rem; margin-bottom: 1rem;"
     >
-      <label for="input-popularX" style="padding-right: 1rem;">First X popular movies</label>
-      <input style="margin-right: 1rem; min-width: 100px; max-width: 200px;" id="input-popularX" type="number" min="0" v-model="popular_x"/>
-      <button id="button-popularX" @click="onClick">Get popular {{ popular_x_repr_string }}</button>
+      <label for="input-movies" style="padding-right: 1rem;">First X movies</label>
+      <input style="margin-right: 1rem; min-width: 100px; max-width: 200px;" id="input-movies" type="number" min="0" v-model="amount"/>
+      <button id="button-movies" @click="onClick">Get {{ amount_repr_string }} movies</button>
     </form>
 
-    <p v-if="displayable_data.length == 0" id="results-popularX">No results yet</p>
+    <p v-if="movies_data.length == 0" id="results-movies">No results yet</p>
     <MovieGrid v-else
-      :movies-data="displayable_data"
+      :movies-data="movies_data"
       @delete="onTriggerDelete"
       @like="onTriggerLike"
     />
