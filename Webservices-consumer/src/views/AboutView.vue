91093f7e1b2ad9ad64f76fe3ffb0c43e4f923ea8
elthomaso <GueutalThomas@outlook.com>
2023-04-17 21:20:49 +0200

    Apply header component.

--- Webservices-consumer/src/views/AboutView.vue
+++ Webservices-consumer/src/views/AboutView.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import MainContentHeader from '@/components/MainContentHeader.vue';
+</script>
+
 <template>
   <main>
-    <h1>About</h1>
+    <MainContentHeader title="About"/>
     <p>
       This vue application acts as the frontend part of the Webservices assignment for the course Distributed systems at the university of Antwerp during the academic year of 2022-2023, by Thomas Gueutal (s0195095).
     </p>
